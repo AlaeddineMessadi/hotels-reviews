aafcdc17aadcecd789fcbb5e612b344a84f05aed
Alaeddine Messadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2018-09-25 22:35:07 +0200

    Add Review Component
    Add review componenet including styling

--- src/app/componenets/Card/Card.js
+++ src/app/componenets/Card/Card.js
@@ -4,16 +4,24 @@ import Image from './Image/Image';
 import InfoBox from './InfoBox/InfoBox';
 import Review from './Review/Review';
 
-import styles from './Card.css';
+import classes from './Card.css';
 
 const card = (props) => {
   return (
-    <div className={styles.card}>
-      <Image image="" alt=""/>
-      <InfoBox title={props.title} city={props.city} rating={props.rating}/>
-      <Review status={props.status} author={props.author} review={props.review} />
+    <div className={ classes.card }>
+      <Image image="" alt="" />
+      <InfoBox title={ props.title } city={ props.city } rating={ props.rating } />
+      {/* <break></break> */ }
+      <div className={ classes.reviews }>
+        <Review status="Negative" author="Alex" review="Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s." />
+        <div className={ classes.break } />
+        <Review status={ props.status } author={ props.author } review={ props.review } />
+      </div>
     </div>
   )
 }
+
 
 export default card;
--- src/app/componenets/Card/Review/Review.js
+++ src/app/componenets/Card/Review/Review.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 import classes from './Review.css';
 
 const review = (props) => {
   return (
-    <div className={classes.review}>
-    <span>{props.satus}</span>
-      <h3>{props.author}</h3>
-      <p>{props.author}</p>
+    <div className={ classes.review }>
+      <div>
+        <span className={ `${classes.sign} ${classes.minus}` }></span>
+      </div>
+      <div>
+        <h3>{ props.author }</h3>
+        <p>{ props.review }</p>
+      </div>
     </div>
   )
 }
 
-export default review
+export default review;
