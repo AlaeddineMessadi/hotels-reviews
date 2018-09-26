432d16ad4a23c5e99e2b3511d2620fcbc63fd876
Alaeddine Messadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2018-09-26 23:57:26 +0200

    Add constant.js for countfetch and dumy images

---
+++ src/app/Utils/Constants.js
@@ -0,0 +1,8 @@
+export const FETCH_COUNT = 5;
+export const IMAGE_URLS = [
+  "https://loremflickr.com/640/480/germany",
+  "https://loremflickr.com/640/480/brazil",
+  "https://loremflickr.com/640/480/usa",
+  "https://loremflickr.com/640/480/india",
+  "https://loremflickr.com/640/480/japan",
+];
--- src/app/components/Carousel/Carousel.js
+++ src/app/components/Carousel/Carousel.js
@@ -2,33 +2,28 @@ import React, { Component } from "react";
 import Arrow from "./Arrow/Arrow";
 import ImageSlide from "./ImageSlide/ImageSlide";
 
-import classes from "./Carousel.css";
-
 /**
  * dummy pictures for testing
  * The are some issue with the images' urls in the api response
  * uncomment this section and the first line in the render method 
  */
-const Urlimages = [
-  "https://loremflickr.com/640/480/germany",
-  "https://loremflickr.com/640/480/brazil",
-  "https://loremflickr.com/640/480/usa",
-  "https://loremflickr.com/640/480/india",
-  "https://loremflickr.com/640/480/japan",
-];
+import { IMAGE_URLS } from '../../Utils/Constants';
+
+import classes from "./Carousel.css";
+
 
 /**
  * Carousel Component
  */
 export default class Carousel extends Component {
   state = {
-    currentImageIndex: 0
+    currentImageIndex: 0,
   };
 
   previousSlide = () => {
     const index =
       this.state.currentImageIndex === 0
-        ? Urlimages.length - 1
+        ? this.props.images.length - 1
         : this.state.currentImageIndex - 1;
 
     this.setState({
@@ -37,9 +32,9 @@ export default class Carousel extends Component {
   };
 
   nextSlide = () => {
-    const lastIndex = Urlimages.length - 1;
+    const lastIndex = this.props.images.length - 1;
     const index =
-      this.state.currentImageIndex === Urlimages.length - 1
+      this.state.currentImageIndex === this.props.images.length - 1
         ? 0
         : this.state.currentImageIndex + 1;
 
@@ -52,7 +47,7 @@ export default class Carousel extends Component {
     //   const Urlimages = this.props.images;
     return (
       <div className={ classes.carousel }>
-        <ImageSlide url={ Urlimages[this.state.currentImageIndex] } />
+        <ImageSlide url={ this.props.images[this.state.currentImageIndex] } />
         <Arrow
           direction="left"
           clickFunction={ this.previousSlide }
